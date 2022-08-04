3832c226c3fa17b0cb8a8b0313eb71628c06acff
Savely <Savka9765@yandex.ru>
2022-08-04 21:18:11 +0300

    Задача 3 Является ли второе число квадратом первогоНаписать программу которая по введенной цифре будет определять день недели

---
+++ Задача 3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Написать программу которая по введенной цифре будет определять день недели
+
+int day_number = new int();
+Console.Write("Введите число дня недели ");
+day_number = Convert.ToInt32(Console.ReadLine());
+
+if (day_number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+else if (day_number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+else if (day_number == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+else if (day_number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+else if (day_number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+else if (day_number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+else if (day_number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+else if (day_number > 7)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
