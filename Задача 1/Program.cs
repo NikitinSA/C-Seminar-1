f5c1d35ce0bdf83f598ba41633d8d7e3ea251790
Savely <Savka9765@yandex.ru>
2022-08-04 20:35:28 +0300

    Задача 1 Квадрат числа

---
+++ Задача 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите прогрумму которая принимает число и выдает его квадрат.
+
+int User_Number = new int();
+Console.WriteLine ("Введите число");
+User_Number=Convert.ToInt32(Console.ReadLine());
+
+int result = (User_Number * User_Number);
+
+Console.WriteLine("Степень числа " + User_Number + " = " + result);
