6247726e15558be9ed737835eb7db4dcdaa114a5
Savely <Savka9765@yandex.ru>
2022-08-04 21:27:41 +0300

    Задача 4 Написать программу, которая на вход принимает одно число, а на выходе показывает все целые числа от  -N до N

---
+++ Задача 4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая на вход принимает одно число, а на выходе показывает все целые числа от  -N до N
+
+int number = new int();
+
+Console.Write("Введите число ");
+number = Convert.ToInt32(Console.ReadLine());
+
+int minus_number = 0-number;
+
+while (minus_number <= number)
+{
+    Console.WriteLine(minus_number);
+    minus_number++;
+}
