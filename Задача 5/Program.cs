e4c60a33461319ef32d265708b4d8eec5256454e
Savely <Savka9765@yandex.ru>
2022-08-04 21:37:34 +0300

    Задача 5 Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого цисла.

---
+++ Задача 5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого цисла.
+
+int number = new int();
+
+Console.Write("Введите положительное трехзначное число ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number <100 || number < 0)
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+else 
+{
+    Console.WriteLine("Последняя цифра вашего числа " + number%10);
+}
