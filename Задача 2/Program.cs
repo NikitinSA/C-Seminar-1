186b5ab4c8a1c49d33c315803b3b2b3fb3dbeeb4
Savely <Savka9765@yandex.ru>
2022-08-04 21:02:21 +0300

    Задача 2 Является ли второе число квадратом первого

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает 2 числа и проверяет, 
+явлется ли второе число квадратом первого
+*/
+
+int user_number_first = new int();
+int user_number_second = new int();
+
+Console.WriteLine("Введите первое число ");
+user_number_first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+user_number_second = Convert.ToInt32(Console.ReadLine());
+
+if (user_number_first * user_number_first == user_number_second)
+
+{
+    Console.WriteLine("Число " + user_number_second + " является квадратом числа " + user_number_first);
+}
+
+else
+{
+    Console.WriteLine("Число " + user_number_second + " не является квадратом числа " + user_number_first);
+}
+
